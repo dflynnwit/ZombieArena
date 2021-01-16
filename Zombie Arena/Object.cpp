a329c084540674b61313b47e79ce137df1fd534d
Denis Flynn <dflynn@wit.ie>
2021-01-16 12:43:20 +0000

    Changes made to Object.h and Object.cpp which worked on Mac/XCode (compiled).
    Changes made to Object.h and Object.cpp which worked on Mac/XCode (compiled).

---
+++ Zombie Arena/Object.cpp
@@ -0,0 +1,21 @@
+//
+//  Object.cpp
+//  AssetManagerv1
+//
+//  Created by Denis Flynn on 03/10/2018.
+//  Copyright © 2018 Denis Flynn. All rights reserved.
+//
+
+#include "Object.h"
+
+
+// Default constructor.
+Object::Object()
+{
+    //TODO: Move to elements that actually use it
+//    // Add a transform component.
+//    AttachComponent<TransformComponent>();
+//    AttachComponent<SpriteComponent>();
+}
+
+
