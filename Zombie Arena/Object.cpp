71a9da5df6b620c9535c817f0e43b9d7fb54a4a3
Denis Flynn <dflynn@wit.ie>
2021-01-16 12:58:30 +0000

    made same changes as in main branch

--- Zombie Arena/Object.cpp
+++ Zombie Arena/Object.cpp
@@ -18,37 +18,4 @@ Object::Object()
 //    AttachComponent<SpriteComponent>();
 }
 
-template<typename T>
-std::shared_ptr<T> Object::AttachComponent() {
-    // First we'll create the component.
-    std::shared_ptr<T> newComponent = std::make_shared<T>();
-    // Check that we don't already have a component of this type.
-    for (std::shared_ptr<Component>& exisitingComponent : m_components)
-    {
-        if (std::dynamic_pointer_cast<T>(exisitingComponent))
-        {
-            // If we do replace it.
-            exisitingComponent = newComponent;
-            return newComponent;
-        } }
-
-    // The component is the first of its type so add it.
-    m_components.push_back(newComponent);
-    // Return the new component.
-    return newComponent;
-}
-
-template<typename T>
-std::shared_ptr<T> Object::GetComponent() {
-    // Check that we don't already have a component of this type.
-    for (std::shared_ptr<Component> exisitingComponent : m_components)
-    {
-        if (std::dynamic_pointer_cast<T>(exisitingComponent))
-        {
-            return std::dynamic_pointer_cast<T>(exisitingComponent);
-        }
-    }
-
-    return nullptr;
-}
 
